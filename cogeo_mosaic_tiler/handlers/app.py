ad5cac2801ee9bb6b838b1a10ee4037351fdee7d
Kyle Barron <kylebarron2@gmail.com>
2020-08-28 22:20:48 -0600

    Fix multi_point import

--- cogeo_mosaic_tiler/handlers/app.py
+++ cogeo_mosaic_tiler/handlers/app.py
@@ -13,11 +13,10 @@ from boto3.session import Session as boto3_session
 from lambda_proxy.proxy import API
 from rasterio.session import AWSSession
 from rio_tiler.colormap import cmap
-from rio_tiler.io import COGReader
+from rio_tiler.io.cogeo import COGReader, multi_point
 from rio_tiler.mosaic import mosaic_reader
 from rio_tiler.mosaic.methods import defaults
 from rio_tiler.profiles import img_profiles
-from rio_tiler.reader import multi_point
 from rio_tiler.utils import geotiff_options, mosaic_cog_tiler, render
 
 from cogeo_mosaic import version as mosaic_version
